85b4c2f7ce36cb0387ab0edd438966637c30c45c
omar algamel <omeralgamel@gmail.com>
2026-06-03 00:52:40 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-tanstackStart: {
-ssr: false,
-
-server: {
-  entry: "server",
-},
-
-prerender: {
-  crawlLinks: true,
-  routes: ["/"],
-},
-
-},
-
-vite: {
-build: {
-ssr: false,
-},
-},
+  tanstackStart: {
+    server: {
+      entry: "server",
+    },
+  },
 });
